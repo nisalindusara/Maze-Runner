5c38d4eb0117415213346702f575359c03ceed5d
Nisal <91654417+nisalindusara@users.noreply.github.com>
2025-09-08 14:04:07 +0530

    add cell struct and player struct to .h

--- game.h
+++ game.h
@@ -1,6 +1,55 @@
 #ifndef GAME_H
 #define GAME_H
 
+//Library inclusion 
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
+//Enum declarations
+typedef enum {NORMAL_CONSUMABLE, NORMAL_BONUS, WALL, STAIR_START, STAIR_END, POLE_ENTER, POLE_EXIT, STARTING_AREA, FLAG,
+                BHAWANA} CellType;
+typedef enum {NORMAL_BONUS, FOOD_POISONING, TRIGGER, HAPPY, DISORIENT} BhawanaCellType;
+typedef enum {NORTH, SOUTH, EAST, WEST, UNCHANGED} Direction;
+typedef enum{ ACTIVE, BLOCKED, STARTING_AREA} PlayerState;
+
+//Union declarations
+
+typedef union 
+{
+    TeleportData teleport;
+    MovementPointData movementpoint;
+} ValidCellData;
+
+//Structure declarations
+
+typedef struct{
+    int dest_floor;
+    int dest_width;
+    int dest_length;
+} TeleportData;
+
+typedef struct{
+    char factor;
+    int value;
+} MovementPointData;
+
+typedef struct{
+    int floor;
+    int width;
+    int length;
+    bool is_valid;
+    CellType celltype;
+    ValidCellData data;
+}Cell;
+
+typedef struct{
+    char name[20];
+    Direction direction;
+    PlayerState player_state;
+    int throw_count;
+    int move_value;
+    Cell* player_pos;
+} Player;
 
 #endif
