950d2cbea998d7a526bdb6e673e3fb44a94166b5
Nisal <91654417+nisalindusara@users.noreply.github.com>
2025-09-08 14:18:58 +0530

    add stair direction enum update player and cell structures add game round variable

--- game.h
+++ game.h
@@ -11,7 +11,8 @@ typedef enum {NORMAL_CONSUMABLE, NORMAL_BONUS, WALL, STAIR_START, STAIR_END, POL
                 BHAWANA} CellType;
 typedef enum {NORMAL_BONUS, FOOD_POISONING, TRIGGER, HAPPY, DISORIENT} BhawanaCellType;
 typedef enum {NORTH, SOUTH, EAST, WEST, UNCHANGED} Direction;
-typedef enum{ ACTIVE, BLOCKED, STARTING_AREA} PlayerState;
+typedef enum {ACTIVE, BLOCKED, STARTING_AREA} PlayerState;
+typedef enum {UPDOWN, UP, DOWN} StairDirection;
 
 //Union declarations
 
@@ -19,7 +20,7 @@ typedef union
 {
     TeleportData teleport;
     MovementPointData movementpoint;
-} ValidCellData;
+} SpecialCellData;
 
 //Structure declarations
 
@@ -40,16 +41,20 @@ typedef struct{
     int length;
     bool is_valid;
     CellType celltype;
-    ValidCellData data;
+    SpecialCellData data;
 }Cell;
 
 typedef struct{
-    char name[20];
+    char name;
     Direction direction;
+    Cell* starting_cell;
     PlayerState player_state;
     int throw_count;
     int move_value;
     Cell* player_pos;
 } Player;
+
+//Variable declarations
+extern int game_round;
 
 #endif
